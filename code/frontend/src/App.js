c7ef5779c9a72d71e6e30fd5a0fb51c70777a776
Aditya Saxena <143660798+adisaxena15@users.noreply.github.com>
2025-04-06 14:20:19 -0500

    pushing changes

---
+++ code/frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-yellow-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-yellow-800">Tailwind is finally working 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
